ed16b928548617f699b1a88123ed53974ce0e06a
Andriy Iwanyuk <andriy.iwanyuk@gmail.com>
2019-01-08 01:02:05 +0200

    add table in order to display test results

--- src/org/gmdh/neuro/fuzzy/Controller.java
+++ src/org/gmdh/neuro/fuzzy/Controller.java
@@ -1,11 +1,17 @@
 package org.gmdh.neuro.fuzzy;
 
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
-import javafx.scene.control.Slider;
-import javafx.scene.control.Spinner;
-import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import lombok.Data;
 import org.gmdh.neuro.fuzzy.gmdh.GmdhNeuroFuzzyNetwork;
 import org.gmdh.neuro.fuzzy.gmdh.config.GmdhConfig;
 import org.gmdh.neuro.fuzzy.gmdh.data.DataEntry;
@@ -13,6 +19,7 @@ import org.gmdh.neuro.fuzzy.gmdh.data.DataSet;
 import org.gmdh.neuro.fuzzy.gmdh.data.NetworkData;
 import org.gmdh.neuro.fuzzy.reader.DataReader;
 
+import java.io.File;
 import java.util.List;
 
 public class Controller {
@@ -27,18 +34,50 @@ public class Controller {
     private Spinner<Integer> bestNeuronCount;
     @FXML
     private LineChart lineChart;
+    @FXML
+    private LineChart lineChart2;
+    @FXML
+    private Spinner<Integer> inputNumber;
 
-    private DataReader dataReader = new DataReader();
+    @FXML
+    private TableView tableView;
+    @FXML
+    private TableColumn pointTab;
+    @FXML
+    private TableColumn realValueTab;
+    @FXML
+    private TableColumn predictedValueTab;
+    @FXML
+    private TableColumn deltaTab;
+    @FXML
+    private TableColumn squaredDeltaTab;
+
+    private FileChooser fileChooser;
+    private GmdhConfig gmdhConfig;
+    private DataReader dataReader;
+    private DataSet currentDataSet;
     private GmdhNeuroFuzzyNetwork network;
 
     @FXML
     public void initialize() {
-        SpinnerValueFactory.IntegerSpinnerValueFactory mFunctionsValueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 10);
+        SpinnerValueFactory.IntegerSpinnerValueFactory mFunctionsValueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 10, 3);
         mFunctionsPerInput.setValueFactory(mFunctionsValueFactory);
         SpinnerValueFactory.DoubleSpinnerValueFactory learningRateValueFactory = new SpinnerValueFactory.DoubleSpinnerValueFactory(0.0, 3.0, 0.5, 0.25);
         learningRate.setValueFactory(learningRateValueFactory);
-        SpinnerValueFactory.IntegerSpinnerValueFactory bestNeuronCountValueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 1000);
+        SpinnerValueFactory.IntegerSpinnerValueFactory bestNeuronCountValueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 1000, 200);
         bestNeuronCount.setValueFactory(bestNeuronCountValueFactory);
+        SpinnerValueFactory.IntegerSpinnerValueFactory inputNumberValueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 20, 4);
+        inputNumber.setValueFactory(inputNumberValueFactory);
+
+        pointTab.setCellValueFactory(new PropertyValueFactory<ErrorEntry, Integer>("point"));
+        realValueTab.setCellValueFactory(new PropertyValueFactory<ErrorEntry, Double>("realValue"));
+        predictedValueTab.setCellValueFactory(new PropertyValueFactory<ErrorEntry, Double>("predictedValue"));
+        deltaTab.setCellValueFactory(new PropertyValueFactory<ErrorEntry, Double>("delta"));
+        squaredDeltaTab.setCellValueFactory(new PropertyValueFactory<ErrorEntry, Double>("squaredDelta"));
+
+        dataReader = new DataReader();
+        fileChooser = new FileChooser();
+        tableView.setEditable(true);
     }
 
     @FXML
@@ -49,10 +88,21 @@ public class Controller {
         gmdhConfig.setLearnTestRatio(learnTestRatio.getValue());
         gmdhConfig.setRegressorsCount(2);
         gmdhConfig.setMFunctionsPerInput(mFunctionsPerInput.getValue());
-        DataSet dataSet = dataReader.loadDataSet((int) gmdhConfig.getLearnTestRatio());
         network = new GmdhNeuroFuzzyNetwork(gmdhConfig);
-        network.buildGmdhStructure(dataSet.getTrainData(), dataSet.getTestData());
-        drawMseChart(dataSet.getTestData());
+        NetworkData testData = currentDataSet.getTestData();
+        network.buildGmdhStructure(currentDataSet.getTrainData(), testData);
+
+        drawMseChart(testData);
+        drawComparisonChart(testData);
+        fillTable(testData);
+    }
+
+    @FXML
+    public void onLoadDataAction() {
+        File file = fileChooser.showOpenDialog(new Stage());
+        if (file != null) {
+            currentDataSet = dataReader.loadDataSet(file, (int) learnTestRatio.getValue(), inputNumber.getValue());
+        }
     }
 
     private void drawMseChart(NetworkData testData) {
@@ -63,5 +113,88 @@ public class Controller {
             series.getData().add(new XYChart.Data<>(x, dataEntries.get(x).getResult() - predictedValues[x]));
         }
         lineChart.getData().add(series);
+    }
+
+    private void drawComparisonChart(NetworkData testData) {
+        List<DataEntry> dataEntries = testData.getDataEntries();
+        final XYChart.Series<Integer, Double> realSeries = new XYChart.Series<>();
+        final XYChart.Series<Integer, Double> predictedSeries = new XYChart.Series<>();
+        double[] predictedValues = network.getPredictedValues(testData);
+        for (int x = 0; x < dataEntries.size(); ++x) {
+            realSeries.getData().add(new XYChart.Data<>(x, dataEntries.get(x).getResult()));
+            predictedSeries.getData().add(new XYChart.Data<>(x, predictedValues[x]));
+        }
+        lineChart2.getData().add(realSeries);
+        lineChart2.getData().add(predictedSeries);
+    }
+
+    private void fillTable(NetworkData testData) {
+        ObservableList<ErrorEntry> entries = FXCollections.observableArrayList();
+        List<DataEntry> dataEntries = testData.getDataEntries();
+        double[] predictedValues = network.getPredictedValues(testData);
+        for (int x = 0; x < dataEntries.size(); ++x) {
+            double realValue = dataEntries.get(x).getResult();
+            double predictedValue = predictedValues[x];
+            double delta = Math.abs(realValue - predictedValue);
+            entries.add(new ErrorEntry(x, realValue, predictedValue, delta, Math.pow(delta, 2)));
+        }
+        tableView.setItems(entries);
+    }
+
+    @Data
+    public static class ErrorEntry {
+        private final SimpleIntegerProperty point;
+        private final SimpleDoubleProperty realValue;
+        private final SimpleDoubleProperty predictedValue;
+        private final SimpleDoubleProperty delta;
+        private final SimpleDoubleProperty squaredDelta;
+
+        public ErrorEntry(int point, double realValue, double predictedValue, double delta, double squaredDelta) {
+            this.point = new SimpleIntegerProperty(point);
+            this.realValue = new SimpleDoubleProperty(realValue);
+            this.predictedValue = new SimpleDoubleProperty(predictedValue);
+            this.delta = new SimpleDoubleProperty(delta);
+            this.squaredDelta = new SimpleDoubleProperty(squaredDelta);
+        }
+
+        public int getPoint() {
+            return point.get();
+        }
+
+        public void setPoint(int point) {
+            this.point.set(point);
+        }
+
+        public double getRealValue() {
+            return realValue.get();
+        }
+
+        public void setRealValue(double realValue) {
+            this.realValue.set(realValue);
+        }
+
+        public double getPredictedValue() {
+            return predictedValue.get();
+        }
+
+        public void setPredictedValue(double predictedValue) {
+            this.predictedValue.set(predictedValue);
+        }
+
+        public double getDelta() {
+            return delta.get();
+        }
+
+        public void setDelta(double delta) {
+            this.delta.set(delta);
+        }
+
+        public double getSquaredDelta() {
+            return squaredDelta.get();
+        }
+
+        public void setSquaredDelta(double squaredDelta) {
+            this.squaredDelta.set(squaredDelta);
+        }
     }
 }
--- src/org/gmdh/neuro/fuzzy/reader/DataReader.java
+++ src/org/gmdh/neuro/fuzzy/reader/DataReader.java
@@ -1,33 +1,67 @@
 package org.gmdh.neuro.fuzzy.reader;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.gmdh.neuro.fuzzy.gmdh.data.DataEntry;
 import org.gmdh.neuro.fuzzy.gmdh.data.DataSet;
 import org.gmdh.neuro.fuzzy.gmdh.data.NetworkData;
 import org.gmdh.neuro.fuzzy.utils.NormalizationUtils;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class DataReader {
 
-    private static final int ATTRIBUTES = 7;
+    private HSSFWorkbook workbook;
 
-    public DataSet loadDataSet(int trainSetPercent) {
+    public DataSet loadDefaultDataSet(int trainSetPercent, int attributesCount) {
         List<DataEntry> dataEntries = new ArrayList<>();
         try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream("/economic.dat")));
             while (true) {
-                reader.readLine();
                 String line = reader.readLine();
                 if (line == null) break;
-                DataEntry dataObject = parse(line);
+                DataEntry dataObject = parse(line, attributesCount);
                 dataEntries.add(dataObject);
             }
         } catch (Exception e) {
         }
 
+        return splitDataSetOnTrainTest(trainSetPercent, dataEntries);
+    }
+
+    public DataSet loadDataSet(File file, int trainSetPercent, int attributesCount) {
+        List<DataEntry> dataEntries = new ArrayList<>();
+        try {
+            workbook = new HSSFWorkbook(new FileInputStream(file));
+            HSSFSheet sheet = workbook.getSheetAt(0);
+            int counter = 0;
+
+            while (counter < sheet.getPhysicalNumberOfRows()) {
+                double[] inputs = new double[attributesCount];
+                DataEntry dataEntry = new DataEntry();
+                for (int i = 0; i < attributesCount; ++i) {
+                    HSSFRow line = sheet.getRow(counter);
+                    inputs[i] = Double.parseDouble(line.getCell(0).toString());
+                    ++counter;
+                }
+                dataEntry.setRegressors(inputs);
+                String result = sheet.getRow(counter).getCell(0).toString();
+                ++counter;
+                dataEntry.setResult(Double.parseDouble(result));
+                dataEntries.add(dataEntry);
+            }
+        } catch (Exception ex) {}
+        return splitDataSetOnTrainTest(trainSetPercent, dataEntries);
+    }
+
+    private DataSet splitDataSetOnTrainTest(int trainSetPercent, List<DataEntry> dataEntries) {
         int trainCount = dataEntries.size() * trainSetPercent / 100;
         int testCount = dataEntries.size() - trainCount;
 
@@ -51,10 +85,10 @@ public class DataReader {
         return dataSet;
     }
 
-    private DataEntry parse(String line) {
+    private DataEntry parse(String line, int attributesCount) {
         DataEntry dataEntry = new DataEntry();
         String[] tokens = StringUtils.split(line);
-        double[] input = new double[ATTRIBUTES];
+        double[] input = new double[attributesCount];
         for (int i = 0; i < tokens.length - 1; i++) {
             input[i] = Double.parseDouble(tokens[i]);
         }
